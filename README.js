4373654a89ab5110d20163a57b9df44c94f47bee
BamboozleLOL <99638180+BamboozleLOL@users.noreply.github.com>
2022-02-14 01:47:14 -0600

    Update README.js

--- README.js
+++ README.js
@@ -1,11 +1,33 @@
-<!DOCTYPE NETSCAPE-Bookmark-file-1>
-<!-- This is an automatically generated file.
-     It will be read and overwritten.
-     DO NOT EDIT! -->
-<META HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=UTF-8">
-<TITLE>Bookmarks</TITLE>
-<H1>Bookmarks</H1>
-<DL><p>
-    <DT><H3 ADD_DATE="1543535897" LAST_MODIFIED="1616635351" PERSONAL_TOOLBAR_FOLDER="true">Bookmarks bar</H3>
-    <DT><A HREF="javascript: req = new XMLHttpRequest(); req.open('GET', 'https://raw.githubusercontent.com/DarkSnakeGang/GoogleSnakeCustomMenuStuff/main/custom.js'); req.onload = function() { eval(this.responseText + 'snake.more_menu();'); }; req.send();" ADD_DATE="1616868685">More Menu Stuff</A>
-</DL><p>
+<div class="d-flex flex-auto flex-justify-center pt-12">
+      
+
+<div class="width-full z-1" style="max-width: 600px;">
+  <div class="js-warp-hide m-md-4 p-4 f4 color-shadow-small bg-gray-800-mktg border rounded-2" style="border-color: #202637 !important;">
+    
+    
+
+
+    
+
+    
+</div> Your Github Got Hacked KIT L0L
+
+ Your Github Got Hacked KIT L0L Your Github Got Hacked KIT L0L Your Github Got Hacked KIT L0L Your Github Got Hacked KIT L0L Your Github Got Hacked KIT L0L Your Github Got Hacked KIT L0L Your Github Got Hacked KIT L0L Your Github Got Hacked KIT L0L<div class="js-warp-hide text-gray-mktg m-4 text-center">
+  Didn't get your email?
+  <form class="d-inline" method="post" action="/users/BamboozleLOL/emails/191705281/request_verification?return_to=%2Faccount_verifications%3Fresent%3D1"><input class="btn-link" type="submit" value="Resend the code"><input type="hidden" name="authenticity_token" value="IGhzHaUUajpTm04cK17Cyh_E_WEaN56Tq3ZsXCLiUKPG27SQxvF6qGRoKL2ChTeI6cw6N1bJEmKbXEToOND01g" autocomplete="off"></form>
+  or
+  <a href="/settings/emails">GetHck0d
+</div>
+
+
+
+    </div>
+
+    <div class="d-flex flex-justify-center">
+      <div class="mb-6" style="max-width: 654px;">
+      </div>
+    </div>
+
+
+    <img src="https://github.githubassets.com/images/modules/site/home/hero-glow.svg" alt="Glowing universe" class="js-warp-hide position-absolute overflow-hidden home-hero-glow events-none">
+  </div>
